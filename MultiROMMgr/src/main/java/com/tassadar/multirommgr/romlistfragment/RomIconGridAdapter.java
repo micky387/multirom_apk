1aa1c6c126c749308c979ec4a19f6eb8075c84e6
Martinusbe <martinusbe@gmail.com>
2016-08-14 08:52:41 +0200

    add tipsy icon (1/2)
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- MultiROMMgr/src/main/java/com/tassadar/multirommgr/romlistfragment/RomIconGridAdapter.java
+++ MultiROMMgr/src/main/java/com/tassadar/multirommgr/romlistfragment/RomIconGridAdapter.java
@@ -50,7 +50,8 @@ public class RomIconGridAdapter extends BaseAdapter {
             R.drawable.romic_linux,
             R.drawable.romic_firefox,
             R.drawable.romic_slimkat,
-            R.drawable.romic_validus
+            R.drawable.romic_validus,
+            R.drawable.romic_tipsy
     };
 
     public RomIconGridAdapter(Context c) {
